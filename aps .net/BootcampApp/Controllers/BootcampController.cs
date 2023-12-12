c6ba4736a2aa270cf75df203ae8358bad91d0f85
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2023-12-12 15:32:01 +0300

    updated asp netcore project

--- aps .net/BootcampApp/Controllers/BootcampController.cs
+++ aps .net/BootcampApp/Controllers/BootcampController.cs
@@ -4,14 +4,17 @@ using Microsoft.AspNetCore.Mvc;
 namespace BootcampApp.Controllers;
 
     public class BootcampController : Controller{
+
+        public IActionResult Details(int? id)
+        {
+            if(id==null){
+                return Redirect("/Bootcamp/List");
+            }
+            var kurs = Repository.GetById(id);
+            return View(kurs);
+        }
         public IActionResult List()
         {
-            var kurslar = new List<Bootcamp>(){
-                new Bootcamp(){Id = 1, Title = "İlk Kurs", Image = "1.jpeg", Description = "İlk kursun açıklaması."},
-                new Bootcamp(){Id = 2, Title = "İkinci Kurs", Image = "2.jpeg", Description = "İkincikursun açıklaması."},
-                new Bootcamp(){Id = 3, Title = "Üçüncü Kurs", Image = "3.jpeg", Description = "Üçüncü kursun açıklaması."},
-                new Bootcamp(){Id = 4, Title = "Dördüncü Kurs", Image = "4.jpeg", Description = "Dördüncü kursun açıklaması."},
-            };
-            return View(kurslar);
+            return View(Repository.Bootcamps);
         }
     }
--- aps .net/BootcampApp/Controllers/HomeController.cs
+++ aps .net/BootcampApp/Controllers/HomeController.cs
@@ -7,13 +7,8 @@ namespace BootcampApp.Controllers;
 public class HomeController : Controller
 {
     public IActionResult Index()
-    {
-        var kurs = new Bootcamp();
-        kurs.Id = 1;
-        kurs.Title = "İlk Kurs";
-        kurs.Image = "1.jpeg";
-        kurs.Description = "İlk kursun açıklaması.";
-        return View(kurs);
+    {        
+        return View(Repository.Bootcamps);
     }
 
 }
